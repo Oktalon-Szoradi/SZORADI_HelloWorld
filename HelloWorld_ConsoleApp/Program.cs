8535bbc3aae28e0c48d568bff4ab47b099d13804
Oktalon-Szoradi <szoradi.o19@htlwienwest.at>
2022-02-13 23:37:55 +0100

    Output integer overflow behavior and strange floating-point addition

--- HelloWorld_ConsoleApp/Program.cs
+++ HelloWorld_ConsoleApp/Program.cs
@@ -8,6 +8,10 @@ namespace HelloWorld_ConsoleApp
         {
             Console.WriteLine($"Hello {Environment.UserName}!");
             Console.WriteLine("It's nice to meet you!");
+            int max = int.MaxValue;
+            Console.WriteLine($"\n\nint.MaxValue ({max}) + 1 = {max + 1}");
+            Console.WriteLine("\nFloating-Point Numbers:");
+            Console.WriteLine($"0.1 + 0.2 = {0.1 + 0.2}");
         }
     }
 }
