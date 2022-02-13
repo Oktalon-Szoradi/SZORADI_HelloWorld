92c9590561d61b780a44c453f7c666544d13315d
Oktalon-Szoradi <szoradi.o19@htlwienwest.at>
2022-02-13 21:13:55 +0100

    Greet username of currently logged-on user instead of World.

--- HelloWorld_ConsoleApp/Program.cs
+++ HelloWorld_ConsoleApp/Program.cs
@@ -6,7 +6,7 @@ namespace HelloWorld_ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine($"Hello {Environment.UserName}!");
         }
     }
 }
