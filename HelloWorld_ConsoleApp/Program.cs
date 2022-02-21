39f762c876b97e955669f8a342b80f5bd56153e2
Oktalon-Szoradi <szoradi.o19@htlwienwest.at>
2022-02-21 09:27:00 +0100

    Just testing some things (pre- and post-increments, default values)

--- HelloWorld_ConsoleApp/Program.cs
+++ HelloWorld_ConsoleApp/Program.cs
@@ -6,12 +6,26 @@ namespace HelloWorld_ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine($"Hello {Environment.UserName}!");
-            Console.WriteLine("It's nice to meet you!");
-            int max = int.MaxValue;
-            Console.WriteLine($"\n\nint.MaxValue ({max}) + 1 = {max + 1}");
-            Console.WriteLine("\nFloating-Point Numbers:");
-            Console.WriteLine($"0.1 + 0.2 = {0.1 + 0.2}");
+            int i = 0;
+            int j = 2;
+            i = Add(i, j);
+            Console.WriteLine($"i:{i} j:{j}"); // i:3 j:2
+
+            //Console.WriteLine("\nDateTime now:");
+            //if (DateTime.TryParse(DateTime.Now.ToString(), out DateTime d))
+            //    Console.WriteLine(d.ToString());
+
+            Console.WriteLine("\nDateTime default:");
+            if (!DateTime.TryParse("foobar", out DateTime e))
+                Console.WriteLine(e.ToString());
+
+            
+        }
+        static int Add(int n1, int n2)
+        {
+            //      ++0 + 2++
+            //        1 + 2
+            return ++n1 + n2++;
         }
     }
 }
